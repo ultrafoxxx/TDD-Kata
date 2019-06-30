783ce33d200b03ad2f1ecc2df72d853f0dee000b
bigfoxxx <ph.cool97@gmail.com>
2019-06-30 11:38:36 +0200

    First stage.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,25 @@
 
 
 public class Calculator {
+
+    public int add(String numbers){
+
+        String delimiter = ",";
+        int result = 0;
+        String[] arrayOfNumbers = numbers.split(delimiter);
+        int maxNumbers = 2;
+        int iterationLength = arrayOfNumbers.length;
+        if(numbers.length()<iterationLength){
+            iterationLength = numbers.length();
+        }
+        else if(iterationLength > maxNumbers){
+            iterationLength = maxNumbers;
+        }
+
+        for(int i=0;i<iterationLength; i++){
+            result+= Integer.valueOf(arrayOfNumbers[i]);
+        }
+        return result;
+    }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,57 @@
-
-
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUpTests(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void checkForEmptyString(){
+
+        String argument = "";
+        int expectedOutcome = 0;
+
+        int result = calculator.add(argument);
+
+        Assert.assertEquals(expectedOutcome, result);
+
+    }
+
+    @Test
+    public void checkForOneNumber(){
+        String argument = "2";
+        int expectedOutcome = 2;
+
+        int result = calculator.add(argument);
+
+        Assert.assertEquals(expectedOutcome, result);
+    }
+
+    @Test
+    public void checkForTwoNumbers(){
+        String argument = "2,5";
+        int expectedOutcome = 7;
+
+        int result = calculator.add(argument);
+
+        Assert.assertEquals(expectedOutcome, result);
+    }
+
+    @Test
+    public void checkForThreeNumbers(){
+        String argument = "2,5,7";
+        int expectedOutcome = 7;
+
+        int result = calculator.add(argument);
+
+        Assert.assertEquals(expectedOutcome, result);
+    }
+
+
 }
